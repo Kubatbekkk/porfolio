35f178a76f30f08b01001752f780708fc8ac99c2
Kubatbekkk <kubineccastro@gmail.com>
2023-04-14 20:48:00 +0600

    fix: Education layout on mobile

--- src/view/components/Education/index.tsx
+++ src/view/components/Education/index.tsx
@@ -1,29 +1,25 @@
+import { Fragment } from "react";
+import education from "../../../data/education"
+import lang from "../../../data/lang";
+import { useLangStore } from "../../../hooks/lang-state"
 import './style.scss'
-import education from '../../../data/education'
-import { Fragment } from 'react'
-import lang from '../../../data/lang'
-import { useLangStore } from '../../../hooks/lang-state'
 
-const Education = () => {
-  const { lng } = useLangStore()
+function Education() {
+  const { lng } = useLangStore();
 
   return (
-    <section className='education' id='education'>
-      <h3 className="education__title">Education</h3>
+    <section className="education" id="education">
+      <h3 className="education__title">{lang[lng]["education"]}</h3>
       <div className="education__list">
-        {education.map((ed, i) => (
+        {education.map((educ, i) =>
           <Fragment key={i}>
-            <div className="education__year">{ed.year}</div>
+            <div className="education__year">{educ.year}</div>
             <div className="education__right-wrapper">
-              <div className="education__where">
-                {lang[lng][ed.where]}
-              </div>
-              <div className="education__profession">
-                {lang[lng][ed.profession]}
-              </div>
+              <div className="education__where">{lang[lng][educ.where]}</div>
+              <div className="education__profession">{lang[lng][educ.profession]}</div>
             </div>
           </Fragment>
-        ))}
+        )}
       </div>
     </section>
   )
--- src/view/components/main/index.tsx
+++ src/view/components/main/index.tsx
@@ -3,7 +3,8 @@ import Top from '../top'
 import About from '../about'
 import Skills from '../skills'
 import Languages from '../languages'
-import Education from '../Education'
+import Education from '../education'
+
 const Main = () => {
   return (
     <>
