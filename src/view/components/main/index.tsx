137fc6ac178492356cbad910ff691a6b933c5840
Kubatbekkk <kubineccastro@gmail.com>
2023-04-16 18:34:35 +0600

    chore: minor change

--- src/view/components/main/index.tsx
+++ src/view/components/main/index.tsx
@@ -5,7 +5,7 @@ import Skills from '../skills'
 import Languages from '../languages'
 import Courses from '../courses'
 import Projects from '../projects'
-import Edu from '../education'
+import Education from '../education'
 
 const Main = () => {
   return (
@@ -15,7 +15,7 @@ const Main = () => {
       <Skills />
       <Languages />
       <div className="two-blocks">
-        <Edu />
+        <Education />
         <Courses />
       </div>
       <Projects />
