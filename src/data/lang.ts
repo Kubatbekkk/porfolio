b471bd5e08c9916910628952115401ff3ecc5ee3
Kubatbekkk <kubineccastro@gmail.com>
2023-09-04 08:48:36 +0600

    upd: lang.ts, compress imgs

--- src/data/lang.ts
+++ src/data/lang.ts
@@ -1,135 +1,161 @@
 const privateLang = {
   en: {
-    "contacts": "contacts",
-    "about": "about",
-    "skills": "skills",
-    "languages": "languages",
-    "education": "education",
-    "courses": "courses",
-    "projects": "projects",
+    contacts: 'contacts',
+    about: 'about',
+    skills: 'skills',
+    languages: 'languages',
+    education: 'education',
+    courses: 'courses',
+    projects: 'projects',
 
-    "Kubatbek Mirzabek": "Kubatbek Mirzabek",
-    "frontend developer": "frontend developer",
-    "Kyrgyzstan, Bishkek": "Kyrgyzstan, Bishkek",
+    'Kubatbek Mirzabek': 'Kubatbek Mirzabek',
+    'frontend developer': 'frontend developer',
+    'Kyrgyzstan, Bishkek': 'Kyrgyzstan, Bishkek',
 
-    "about1": "I am a <span>frontend developer</span> with a strong background in project development acquired through both learning experiences and over one year of commercial development experience.",
-    "about2": "In the course of training, I have completed more than a dozen different works as part of <span>The Rolling Scopes School</span> training course.",
-    "about3": "I have experience working with various <span>tools</span> and <span>libraries</span>, experience in team development, according to the <span>git-flow</span> methodology.",
-    "about4": "My work, including my commercial experience, is highly appreciated not only by fellow students and school mentors but also by team leads.",
+    about1:
+      'I am a <span>frontend developer</span> with a strong background in project development acquired through both learning experiences and over one year of commercial development experience.',
+    about2:
+      'In the course of training, I have completed more than a dozen different works as part of <span>The Rolling Scopes School</span> training course.',
+    about3:
+      'I have experience working with various <span>tools</span> and <span>libraries</span>, experience in team development, according to the <span>git-flow</span> methodology.',
+    about4:
+      'My work, including my commercial experience, is highly appreciated not only by fellow students and school mentors but also by team leads.',
 
-    "Kyrgyz": "Kyrgyz",
-    "Russian": "Russian",
-    "English": "English",
-    "near-native": "near-native",
-    "native": "native",
-    "upper-intermediate (B2)": "upper-intermediate (B2)",
+    Kyrgyz: 'Kyrgyz',
+    Russian: 'Russian',
+    English: 'English',
+    'near-native': 'near-native',
+    native: 'native',
+    'upper-intermediate (B2)': 'upper-intermediate (B2)',
 
-    "The Rolling Scopes School": "The Rolling Scopes School",
-    "Kyrgyz Economic University": "Kyrgyz Economic University",
-    "JS/Front-end": "JS/Front-end",
-    "Information Technology": "Information Technology",
+    'The Rolling Scopes School': 'The Rolling Scopes School',
+    'Kyrgyz Economic University': 'Kyrgyz Economic University',
+    'JS/Front-end': 'JS/Front-end',
+    'Information Technology': 'Information Technology',
 
-    "completed": "completed",
-    "certificate is pending...": "certificate is pending...",
+    completed: 'completed',
+    'certificate is pending...': 'certificate is pending...',
 
-    "PetStory Online": "PetStory Online",
-    "Tags": "Fifteen Puzzle Game",
-    "DashBoard": "DashBoard",
-    "Songbird": "Songbird",
-    "Online Store": "Online Store",
-    "Gaming Zone": "Gaming Zone",
+    'PetStory Online': 'PetStory Online',
+    Tags: 'Fifteen Puzzle Game',
+    DashBoard: 'DashBoard',
+    Songbird: 'Songbird',
+    'Online Store': 'Online Store',
+    'Gaming Zone': 'Gaming Zone',
 
-    "PetStory Online Desc": "<span>Adaptive site layout</span> (up to 320px) for an online zoo, there are About and Donate pages. Implemented: carousel, scrolling comments, burger menu and modal window with comments in the mobile version, used <span>vanilla JavaScript</span> (without libraries), HTML, CSS.",
+    'PetStory Online Desc':
+      '<span>Adaptive site layout</span> (up to 320px) for an online zoo, there are About and Donate pages. Implemented: carousel, scrolling comments, burger menu and modal window with comments in the mobile version, used <span>vanilla JavaScript</span> (without libraries), HTML, CSS.',
 
-    "Tags Desc": "Implementation of <span>\"Fifteen Puzzle Game\"</span>, additionally implemented saving the game to localStorage and loading previously saved game, 6 game levels. The whole game is written in <span>vanilla JavaScript</span> (without libraries) according to the SPA, without using HTML tags the body tag.",
+    'Tags Desc':
+      'Implementation of <span>"Fifteen Puzzle Game"</span>, additionally implemented saving the game to localStorage and loading previously saved game, 6 game levels. The whole game is written in <span>vanilla JavaScript</span> (without libraries) according to the SPA, without using HTML tags the body tag.',
 
-    "Dashboard Desc": "This project is a React Admin Dashboard that I built using various libraries including Material UI, Nivo Charts, Formik, Yup, FullCalendar, and Data Grid. It features Light and Dark Modes, 4 different types of charts, 3 pages with distinct data tables, a FAQ page, a form page, and integration with a calendar.",
+    'Dashboard Desc':
+      'This project is a React Admin Dashboard that I built using various libraries including Material UI, Nivo Charts, Formik, Yup, FullCalendar, and Data Grid. It features Light and Dark Modes, 4 different types of charts, 3 pages with distinct data tables, a FAQ page, a form page, and integration with a calendar.',
 
-    "Songbird Desc": "A simple <span>quiz game </span> for guessing the singing of birds, additionally implemented a gallery of all the birds available in the game. The whole game is written in <span>vanilla JavaScript</span> (without libraries) according to the SPA, without using HTML tags the body tag.",
+    'Songbird Desc':
+      'A simple <span>quiz game </span> for guessing the singing of birds, additionally implemented a gallery of all the birds available in the game. The whole game is written in <span>vanilla JavaScript</span> (without libraries) according to the SPA, without using HTML tags the body tag.',
 
-    "Online Store Desc": "<span>Team development</span> with other one developer. Test online store. There are implemented: filters, search, adding/removing items into/from the basket, fake-purchase, etc. The application is written in <span>vanilla TypeScript</span> (without libraries, Any type and JavaScript) according to the <span>SPA.</span>",
+    'Online Store Desc':
+      '<span>Team development</span> with other one developer. Test online store. There are implemented: filters, search, adding/removing items into/from the basket, fake-purchase, etc. The application is written in <span>vanilla TypeScript</span> (without libraries, Any type and JavaScript) according to the <span>SPA.</span>',
 
-    "RSKanban Desc": "<span>Collaborative development</span> in a team of 3 people. A small application similar to a Kanban board was developed. The entire front-end was written using the <span>React</span> library and <span>TypeScript</span>. <span>Firebase</span> was used for the back-end. Although the project was a team effort, most of the work was done by me due to the lower skill level of my teammates.",
+    'RSKanban Desc':
+      '<span>Collaborative development</span> in a team of 3 people. A small application similar to a Kanban board was developed. The entire front-end was written using the <span>React</span> library and <span>TypeScript</span>. <span>Firebase</span> was used for the back-end. Although the project was a team effort, most of the work was done by me due to the lower skill level of my teammates.',
 
-    "PetStory Online Short Desc": "<span>Adaptive site layout</span> up to 320px. JavaScript, HTML, CSS.",
-    "Tags Short Desc": "<span>Fifteen Puzzle Game</span> with 6 game levels and saving. JavaScript (SPA), CSS.",
-    "Kanban Short Desc": "<span>Kanban Application </span>, co-development. React, TypeScript, Firebase.",
+    'PetStory Online Short Desc':
+      '<span>Adaptive site layout</span> up to 320px. JavaScript, HTML, CSS.',
+    'Tags Short Desc':
+      '<span>Fifteen Puzzle Game</span> with 6 game levels and saving. JavaScript (SPA), CSS.',
+    'Kanban Short Desc':
+      '<span>Kanban Application </span>, co-development. React, TypeScript, Firebase.',
   } as any,
   ru: {
-    "contacts": "контакты",
-    "about": "обо мне",
-    "skills": "навыки",
-    "languages": "языки",
-    "education": "образование",
-    "courses": "курсы",
-    "projects": "проекты",
+    contacts: 'контакты',
+    about: 'обо мне',
+    skills: 'навыки',
+    languages: 'языки',
+    education: 'образование',
+    courses: 'курсы',
+    projects: 'проекты',
 
-    "Kubatbek Mirzabek": "Кубатбек Мирзабек",
-    "frontend developer": "frontend разработчик",
-    "Kyrgyzstan, Bishkek": "Кыргызстан, Бишкек",
+    'Kubatbek Mirzabek': 'Кубатбек Мирзабек',
+    'frontend developer': 'frontend разработчик',
+    'Kyrgyzstan, Bishkek': 'Кыргызстан, Бишкек',
 
-    "about1": "Я <span>frontend разработчик</span> с крепким опытом разработки проектов, накопленным в процессе обучения и более чем годом опыта коммерческой разработки.",
-    "about2": "В процессе обучения мною было выполнено более десятка различных работ, в рамках учебного курса <span>Rolling Scopes School</span>.",
-    "about3": "Имеется опыт работы с различными <span>инструментами</span> и <span>библиотеками</span>, опыт командной разработки, согласно методологии <span>git-flow</span>.",
-    "about4": "Моя работа, включая мой коммерческий опыт, высоко ценится не только среди сокурсников и наставников в школе, но и среди руководителей команды.",
+    about1:
+      'Я <span>frontend разработчик</span> со значительный опытом разработки проектов, накопленным в процессе обучения и более чем годом опыта коммерческой разработки.',
+    about2:
+      'В процессе обучения мною было выполнено более десятка различных работ, в рамках учебного курса <span>Rolling Scopes School</span>.',
+    about3:
+      'Имеется опыт работы с различными <span>инструментами</span> и <span>библиотеками</span>, опыт командной разработки, согласно методологии <span>git-flow</span>.',
+    about4:
+      'Мои работы, включая мой коммерческий опыт, высоко ценятся не только среди сокурсников и наставников в школе, но и среди руководителей команды в коммерческих проектах.',
 
-    "Kyrgyz": "Кыргызский",
-    "Russian": "Русский",
-    "English": "Английский",
-    "near-native": "второй родной",
-    "native": "родной",
-    "upper-intermediate (B2)": "выше среднего (B2)",
+    Kyrgyz: 'Кыргызский',
+    Russian: 'Русский',
+    English: 'Английский',
+    'near-native': 'второй родной',
+    native: 'родной',
+    'upper-intermediate (B2)': 'выше среднего (B2)',
 
-    "The Rolling Scopes School": "The Rolling Scopes School",
-    "Kyrgyz Economic University": "Кыргызский Экономический Университет",
-    "JS/Front-end": "JS/Front-end",
-    "Information Technology": "Информационные технологии",
+    'The Rolling Scopes School': 'The Rolling Scopes School',
+    'Kyrgyz Economic University': 'Кыргызский Экономический Университет',
+    'JS/Front-end': 'JS/Front-end',
+    'Information Technology': 'Информационные технологии',
 
-    "completed": "завершен",
-    "certificate is pending...": "ожидается выдача...",
+    completed: 'завершен',
+    'certificate is pending...': 'ожидается выдача...',
 
-    "PetStory Online": "PetStory Online",
-    "Tags": "Пятнашки",
-    "DashBoard": "DashBoard",
-    "Songbird": "Songbird",
-    "Online Store": "Online Store",
-    "Gaming Zone": "Gaming Zone",
+    'PetStory Online': 'PetStory Online',
+    Tags: 'Пятнашки',
+    DashBoard: 'DashBoard',
+    Songbird: 'Songbird',
+    'Online Store': 'Online Store',
+    'Gaming Zone': 'Gaming Zone',
 
-    "PetStory Online Desc": "<span>Адаптивная верстка</span> сайта (до 320px) для онлайн зоопарка, имеются страниц <span>About</span> и <span>Donate</span>. Реализованы: карусель, прокрутка комментариев, бургер меню и модальное окно с комментариями в мобильной версии, использован <span>чистый JavaScript</span> (без дополнительных библиотек), HTML, CSS.",
+    'PetStory Online Desc':
+      '<span>Адаптивная верстка</span> сайта (до 320px) для онлайн зоопарка, имеются страниц <span>About</span> и <span>Donate</span>. Реализованы: карусель, прокрутка комментариев, бургер меню и модальное окно с комментариями в мобильной версии, использован <span>чистый JavaScript</span> (без дополнительных библиотек), HTML, CSS.',
 
-    "Tags Desc": "Реализация игры <span>\"Пятнашки\"</span>, дополнительно реализовано сохранение игры в localStorage и загрузка ранее сохраненной игры, 6 уровней сложности. Вся игра написана на <span>чистом JavaScript</span> (без дополнительных библиотек) по принципу <span>SPA</span>, без использования HTML тэгов внутри тэга body.",
+    'Tags Desc':
+      'Реализация игры <span>"Пятнашки"</span>, дополнительно реализовано сохранение игры в localStorage и загрузка ранее сохраненной игры, 6 уровней сложности. Вся игра написана на <span>чистом JavaScript</span> (без дополнительных библиотек) по принципу <span>SPA</span>, без использования HTML тэгов внутри тэга body.',
 
-    "Dashboard Desc": "React Admin Dashboard, который я создал, используя различные библиотеки, в том числе Material UI, Nivo Charts, Formik, Yup, FullCalendar и Data Grid. Он включает светлую и темную темы, 4 различных типа графиков, 3 страницы с различными таблицами данных, страницу FAQ, страницу формы и интеграцию с календарем.",
+    'Dashboard Desc':
+      'React Admin Dashboard, который я создал, используя различные библиотеки, в том числе Material UI, Nivo Charts, Formik, Yup, FullCalendar и Data Grid. Он включает светлую и темную темы, 4 различных типа графиков, 3 страницы с различными таблицами данных, страницу FAQ, страницу формы и интеграцию с календарем.',
 
-    "Songbird Desc": "Простая <span>игра-викторина</span> на угадывание пения птиц, дополнительно реализованная галерея всех птиц, имеющихся в игре. Вся игра написана на <span>чистом JavaScript</span> (без дополнительных библиотек) по принципу <span>SPA</span>, без использования HTML тэгов внутри тэга body.",
+    'Songbird Desc':
+      'Простая <span>игра-викторина</span> на угадывание пения птиц, дополнительно реализованная галерея всех птиц, имеющихся в игре. Вся игра написана на <span>чистом JavaScript</span> (без дополнительных библиотек) по принципу <span>SPA</span>, без использования HTML тэгов внутри тэга body.',
 
-    "Online Store Desc": "<span>Совместная разработка</span> в команде из 2-х человек. Тестовый <span>интернет-магазин</span>, реализованы фильтры, поиск, добавление/удаление из корзины товаров, псевдо-покупка и др. Приложение написано на <span>чистом TypeScript</span> (без дополнительных библиотек, универсального типа Any и JavaScript) по принципу <span>SPA</span>, без использования HTML тэгов внутри тэга body.",
+    'Online Store Desc':
+      '<span>Совместная разработка</span> в команде из 2-х человек. Тестовый <span>интернет-магазин</span>, реализованы фильтры, поиск, добавление/удаление из корзины товаров, псевдо-покупка и др. Приложение написано на <span>чистом TypeScript</span> (без дополнительных библиотек, универсального типа Any и JavaScript) по принципу <span>SPA</span>, без использования HTML тэгов внутри тэга body.',
 
-    "RSKanban Desc": "<span>Совместная разработка</span> в команде из 3-х человек. Небольшое приложение наподобие kanban доски. Вся front-end часть написана с использование библиотеки <span>React</span> и <span>TypeScript</span>. Для <span>Back-end</span> был использован <span>Firebase</span>. Несмотря на то что проект был командным, почти вся работа была выполнена мной ввиду более низкого уровня сокомандников.",
-    "PetStory Online Short Desc": "<span>Адаптивная верстка</span> до 320px. JavaScript, HTML, CSS.",
-    "Tags Short Desc": "<span>Игра в пятнашки</span> с 6 уровнями и сохраниением. JavaScript (SPA), CSS.",
-    "Kanban Short Desc": "<span>Канбан приложение</span>, совместная разработка. React, TypeScript, Firebase.",
-  } as any
-}
+    'RSKanban Desc':
+      '<span>Совместная разработка</span> в команде из 3-х человек. Небольшое приложение наподобие kanban доски. Вся front-end часть написана с использование библиотеки <span>React</span> и <span>TypeScript</span>. Для <span>Back-end</span> был использован <span>Firebase</span>. Несмотря на то что проект был командным, почти вся работа была выполнена мной ввиду более низкого уровня сокомандников.',
+    'PetStory Online Short Desc':
+      '<span>Адаптивная верстка</span> до 320px. JavaScript, HTML, CSS.',
+    'Tags Short Desc':
+      '<span>Игра в пятнашки</span> с 6 уровнями и сохраниением. JavaScript (SPA), CSS.',
+    'Kanban Short Desc':
+      '<span>Канбан приложение</span>, совместная разработка. React, TypeScript, Firebase.',
+  } as any,
+};
 
 const lang = {} as any;
 
 lang.ru = new Proxy(privateLang.ru, {
   get(target, key) {
-    if(target && target[key]) {
-      return target[key]
+    if (target && target[key]) {
+      return target[key];
     }
-    return key
-  }
-})
+    return key;
+  },
+});
 
 lang.en = new Proxy(privateLang.en, {
   get(target, key) {
-    if(target && target[key]) {
-      return target[key]
+    if (target && target[key]) {
+      return target[key];
     }
-    return key
-  }
-})
+    return key;
+  },
+});
 
-export default lang
+export default lang;
